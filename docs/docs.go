b435e848e830f493c1bb4adae5b348f3a513bcda
lvlvSSS <nelson.lv.007@outlook.com>
2022-07-03 22:32:27 +0800

    add default value for LoadFlag

--- docs/docs.go
+++ docs/docs.go
@@ -1,9 +1,17 @@
 package docs
 
-import "github.com/urfave/cli/v2"
+import (
+	"github.com/urfave/cli/v2"
+	"os"
+	"path/filepath"
+)
 
 const APP_NAME = "dealer-cli"
 
 const APP_LOAD_YAML = "load-yaml"
 
-var LoadFlag = &cli.StringFlag{Name: "load-yaml"}
+var root, _ = os.Getwd()
+var LoadFlag = &cli.StringFlag{
+	Name:  APP_LOAD_YAML,
+	Value: filepath.Join(root, "configs.yaml"),
+}
